890ec070417500386daae9251e312df829b04942
Gabriel Paschoal <gabrielpaschoal0002@gmail.com>
2026-04-12 14:44:31 -0300

    fix Header background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   const scroll = useScroll();
 
   return (
-    <header className="fixed left-0 top-0 z-3 w-full bg-(--background)">
+    <header className="fixed left-0 top-0 z-3 w-full bg-black">
       <div className="grid grid-cols-[1fr_auto] [grid-template-areas:'b_c'] md:grid-cols-[minmax(44px,auto)_1fr_auto] md:[grid-template-areas:'._b_c'] gap-x-2 p-2">
         {scroll > 250 ? (
           <div className="[grid-area:b] flex justify-center">
