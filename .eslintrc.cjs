11d5e45337a8aefcf81ed45cb1b7f2e7ebe79d13
Gabriel Paschoal <gabrielpaschoal0002@gmail.com>
2026-04-17 15:18:11 -0300

    refactor eslint config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,28 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-  ],
-  ignorePatterns: ["node_modules", ".eslintrc.cjs"],
+  env: { browser: true, node: true, es2020: true },
+  extends: ["eslint:recommended"],
+  ignorePatterns: ["node_modules"],
   parser: "@typescript-eslint/parser",
   rules: {
     "object-curly-spacing": ["error", "always"],
     "key-spacing": ["error", { beforeColon: false, afterColon: true }],
     "arrow-spacing": ["error", { before: true, after: true }],
     "comma-dangle": ["error", "always-multiline"],
-    "no-unused-vars": ["warn", { vars: "all", args: "after-used", ignoreRestSiblings: true }],
+    "no-unused-vars": [
+      "warn",
+      { vars: "all", args: "after-used", ignoreRestSiblings: true },
+    ],
+    "max-len": [
+      "warn",
+      {
+        code: 80,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
   },
 };
