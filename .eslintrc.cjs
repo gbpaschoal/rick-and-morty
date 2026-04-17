951f972a0cba4d891a46f5f90097be1fa28f358a
Gabriel Paschoal <gabrielpaschoal0002@gmail.com>
2026-04-17 14:28:30 -0300

    exclude eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,27 +7,10 @@ module.exports = {
   ignorePatterns: ["node_modules", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   rules: {
-    semi: ["error", "always"],
-    quotes: ["error", "double"],
-    indent: ["error", 2],
     "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "max-len": [
-      "warn",
-      {
-        code: 80,
-        ignoreComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-      },
-    ],
     "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+    "arrow-spacing": ["error", { before: true, after: true }],
     "comma-dangle": ["error", "always-multiline"],
-    "no-multiple-empty-lines": ["error", { max: 1 }],
     "no-unused-vars": ["warn", { vars: "all", args: "after-used", ignoreRestSiblings: true }],
-    "no-console": "warn",
-    "prefer-const": "error",
   },
 };
